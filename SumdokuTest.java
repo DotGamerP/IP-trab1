56171c5030674cf313d7befeda2ae822fac47274
Kamitsue <fishtiago2006@gmail.com>
2024-11-15 02:24:12 +0000

    .

--- SumdokuTest.java
+++ SumdokuTest.java
@@ -406,22 +406,41 @@ public class SumdokuTest {
 		public static boolean isValidForPuzzle(SumdokuGrid grid){
 
 			int gridSize = grid.size();
-			int rowRepeated = 0;
 
-			for(int i = 1; i <= gridSize; i++)
-
-				for(int j = 1; j < gridSize; j++)
-
-					for(int k = 2; k <= gridSize; k++)
-
-						if(grid.value(i, j) == grid.value(i, k))
-							rowRepeated--;
-
-			if(rowRepeated < 0)
-				return true;
-
-			else 
+			if(grid == null || gridSize <= 0)
 				return false;
+
+			
+			// Para verificar se o número de cada posição está repetida em cada linha
+			for(int row = 1; row <= gridSize; row++) {
+
+				for(int col = 1; col < gridSize; col++) {
+
+					int value = grid.value(row, col);
+
+					if(grid.value(row, col) < 1 || grid.value(row, col) > gridSize)
+						return false;
+
+					if(value == grid.value(row, col + 1))
+						return false;
+				}
+			}
+
+			// Para verificar se o número de cada posição está repetida em cada coluna
+			for(int col = 1; col <= gridSize; col++) {
+
+				for(int row = 1; row < gridSize; row++) {
+
+					int value = grid.value(row, col);
+
+					if(value == grid.value(row + 1, col))
+						return false;
+
+				}
+			}
+			
+			// Caso nenhuma destas condições sejam acionadas então o puzzle é válido
+			return true;
 		}
 
 		public static boolean isValidForPuzzle(GridGroups groups){
